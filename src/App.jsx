736113fff67fe6f6afb0ac71724c6ea23f249304
KauaS4ntiago <kauaandrade1806@gmail.com>
2026-05-27 13:50:00 -0300

    Implementado conceito de useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { useState } from 'react'
 
 function App() {
+const [tasks, setTasks] = useState([
+  { id: 1, text: 'Learn JSX', done: false },
+  { id: 2, text: 'Learn components', done: true },
+  { id: 3, text: 'Learn props', done: true },
+])
   return (
       <div className="container">
         <Header title="Minhas tarefas"/>
-        <p>Em breve, nossa lista vai aparecer aqui.</p>
+        {tasks.map((task) => <p key={task.id}>{task.text}</p>)}
         <Footer name="Kauã"/>
       </div>  
   )
