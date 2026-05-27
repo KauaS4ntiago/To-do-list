efadbd5fb02e95efcb668dcbf2cf776c5346e1eb
KauaS4ntiago <kauaandrade1806@gmail.com>
2026-05-26 22:39:29 -0300

    Criação do projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+
+function App() {
+  // Isso é JSX — parece HTML, mas é JavaScript
+  return (
+    <div className="container">
+      <h1>Minhas Tarefas</h1>
+      <p>Em breve, nossa lista vai aparecer aqui.</p>
+    </div>
+  )
+}
+
+export default App
